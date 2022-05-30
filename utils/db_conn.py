e9ea756cf3f5514c191d2455e738dc8ea217e292
joaomsimoes <joaoms1@gmail.com>
2022-05-30 10:36:42 +0100

    v1

---
+++ utils/db_conn.py
@@ -0,0 +1,27 @@
+from utils.keys import DB
+import logging
+from datetime import datetime
+from sqlalchemy import create_engine
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def conn_database():
+    return create_engine(DB)
+
+
+def query(prod=None, values=None):
+    try:
+        engine = conn_database()
+        connection = engine.raw_connection()
+        cursor = connection.cursor()
+        cursor.callproc(prod, values)
+        results = list(cursor.fetchall())
+        connection.commit()
+        connection.close()
+
+        return results
+
+    except Exception as e:
+        now = datetime.now()
+        logging.exception(now.strftime('%H:%M:%S') + " ; " + str(e) + "\n")
